eeb21a6e4317012080b39dadd581566b98f04827
litttlechin <good.life.is.for.me@gmail.com>
2018-10-02 12:01:20 +0800

    up

--- HomeWork/20181002/affine-cipher-100.py
+++ HomeWork/20181002/affine-cipher-100.py
@@ -7,30 +7,20 @@ def trans():
     str(atext)
     for i in range(27):
         i = i * 4 + 15
-        i %= 26
+        i %= 27
         atext += text[i]
     return atext
 
 
-def caesar(input_string, rot):
+def caesar(input_string):
     output_string = ""
-    num = input_string.find(input_string[rot])
-    num *= 4
-    num += 5
-    num %= 27
-    output_string = str(num) + input_string[num]
-    # for i in range(len(input_string)):
-    #     if input_string[i].isalnum():
-    #         idx = (caesaralpha.find(input_string[i]) + rot) % len(caesaralpha)
-    #         output_string += caesaralpha[idx]
-    #     else:
-    #         output_string += input_string[i]
+    str(output_string)
+    for i in range(len(input_string)):
+        num = trans().find(input_string[i])
+        output_string += text[num]
     return output_string
 
 enc = 'ifpmluglesecdlqp_rclfrseljpkq' # encrypt data
 
-# for i in range(len(caesaralpha)):
-#     print(caesar(enc, i))
 
-
-print(trans())
+print(caesar(enc))
